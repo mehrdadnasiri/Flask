d5f372c6da1ad2437127fcea6e0c7b39ad41a442
Mehrdad Nasiri <m.nasiri@HUB.LOCAL>
2022-01-09 12:27:21 +0330

    add controller and resource

--- authz/resource/apiv1/__init__.py
+++ authz/resource/apiv1/__init__.py
@@ -1,1 +1,17 @@
 from authz.authz import apiv1 as api
+from authz.resource.apiv1.user import UserResource
+
+api.add_resource(
+    UserResource,
+    "/users",
+    method=["GET","POST"],
+    endpoint="users"
+)
+
+api.add_resource(
+    UserResource,
+    "/users/<userid>",
+    method=["GET","PATCH","DELETE"],
+    endpoint="user"
+)
+
