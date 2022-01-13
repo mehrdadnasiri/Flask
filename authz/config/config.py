3401669f0702e0cda044c22fb352cea9080d6d94
Mehrdad Nasiri <m.nasiri@HUB.LOCAL>
2022-01-13 11:30:10 +0330

    add config file for DB

--- authz/config/config.py
+++ authz/config/config.py
@@ -1,5 +1,7 @@
 from os import environ
 class Config:
+    ############################Application Config ###################################
+
     ENV = environ.get("TECHLAND_AUTHZ_ENV", "production")
 
     DEBUG = bool(int(environ.get("TECHLAND_AUTHZ_DEBUG","0")))
@@ -9,6 +11,9 @@ class Config:
     SECRET_KEY=  environ.get("TECHLAND_AUTHZ_SECRET_KEY", "HARD-HARD-HARD-SECRET-KEY")
 
     
-
-
+    ############################Database Config ###################################
+    SQLALCHEMY_DATABASE_URI = environ.get("TECHLAND_AUTHZ_DATABASE_URI", "None")
+    SQLALCHEMY_ECHO = DEBUG
+    SQLALCHEMY_RECORD_QUERIES = DEBUG
+    SQLALCHEMY_TRACK_MODIFICATIONS = DEBUG
 
