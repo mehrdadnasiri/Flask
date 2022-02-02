0a57115dcb143a728ca7da88284d0ef36d4dff26
Mehrdad <mehrdad.nasiiri@gmail.com>
2022-02-02 16:41:19 +0330

    add now and expires in util

---
+++ authz/util/expires.py
@@ -0,0 +1,6 @@
+from datetime import timedelta
+
+from authz.config import Config
+from authz.util import now 
+def user_expires_at():
+    return now + timedelta(days=Config.USER_DEFAULT_EXPIRY_TIME)
