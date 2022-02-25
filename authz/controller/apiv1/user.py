6040e95b26e9529e31bd103ea68e27a97f053300
Mehrdad <mehrdad.nasiiri@gmail.com>
2022-02-25 20:49:13 +0330

    complete controler

--- authz/controller/apiv1/user.py
+++ authz/controller/apiv1/user.py
@@ -3,7 +3,7 @@ from flask import request
 from authz.authz import db
 from authz.model import User
 from authz.schema.apiv1 import UserSchema
-from authz.util import jsonify
+from authz.util import user_expires_at, jsonify, now
 
 class UserController:
 
@@ -20,11 +20,22 @@ class UserController:
         ) 
 
     def get_user(user_id):
-        return jsonify(status=501, code=107) #Not Implemented
-
-    def create_user():
         if request.content_type != "application/json":
-            return jsonify(status=415, code=101)# invalid media type 
+            return jsonify(status=415, code=101)#invalid media type
+        try:
+            user= User.query.get(user_id)#find the user
+        except Exception as e:
+            return jsonify(status=500, code=102)#database error
+        if user is None :
+            return jsonify (status=404, code=103)#User is not found
+        user_schema = UserSchema()
+        return jsonify(
+            {"user":user_schema.dump(user)}
+        )
+
+    def create_user(): 
+        if request.content_type != "application/json":
+            return jsonify(status=415, code=101)#invalid media type
         user_schema = UserSchema(only=["username", "password"])
         try:
             user_data = user_schema.load(request.get_json()) #Read and validate user data
@@ -47,13 +58,64 @@ class UserController:
             db.session.commit() #Execute INSERT command.
         except Exception as e:
             db.session.rollback()
+            return jsonify(status=500 , code=102)#Database error
         user_schema = UserSchema()
         return jsonify(
             {"user": user_schema.dump(user)}, status=201
         )
 
     def update_user(user_id):
-        return jsonify(status=501, code=107) #Not Implemented
- 
+        if request.content_type != "application/json":
+            return jsonify(status=415, code=101)#invalid media type
+        user_schema =UserSchema(only=["password"], unknown="include")
+        try:
+            user_data = user_schema.load(request.get_json())#Read and validate user data.
+        except Exception as e:
+            return jsonify(status=400, code=104)
+        if len(user_data) != 2:
+            return jsonify(status=400, code=104)
+        if "password" not in user_data or "oldpassword" not in user_data:
+            return jsonify(status=400, code=104)
+        if not user_data.get("password") or not user_data.get("oldpassword"):
+            return jsonify(status=400, code=105)#empty data
+        try:
+            user= User.query.get(user_id)#find the user
+        except Exception as e:
+            return jsonify(status=500, code=102)#database error
+        if user is None :
+            return jsonify (status=404, code=103)#User is not found
+        if user.password != user_data.get("oldpassword"):
+            return jsonify(status=403, code=111)
+        
+        user.password = user_data.get("password") #set a new password
+        user.expires_at = user_expires_at()
+        user.last_change_at = now()
+        user.failed_auth_at = None
+        user.failed_auth_count= 0
+        try:
+            db.session.commit() #Execute Update command.
+        except Exception as e:
+            db.session.rollback()
+            return jsonify(status=500 , code=102)#Database error
+        user_schema = UserSchema()
+        return jsonify(
+            {"user": user_schema.dump(user)}
+        )
+
     def delete_user(user_id):
-        return jsonify(status=501, code=107) #Not Implemented
+        if request.content_type != "application/json":
+            return jsonify(status=415, code=101)#invalid media type
+        try:
+            user= User.query.get(user_id)#find the user
+        except Exception as e:
+            return jsonify(status=500, code=102)#database error
+        if user is None :
+            return jsonify (status=404, code=103)#User is not found
+        db.session.delete(user)
+        try:
+            db.session.commit() #Execute DELETE command.
+        except Exception as e:
+            db.session.rollback()
+            return jsonify(status=500 , code=102)#Database error 
+        return jsonify()#user was deleted successfuly
+        
